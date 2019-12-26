333ee99c6f889ad41a2cffaf7e6f4af4a44ab6c8
akash kumar <akash@iofactory.com>
2019-12-27 01:18:38 +0530

    contact me page

---
+++ src/Screens/ContactMe/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ContactMe extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={};
+    }
+
+    render(){
+        return(
+            <div>
+                Contact Me
+            </div>
+        )
+    }
+}
+
+export default ContactMe;
