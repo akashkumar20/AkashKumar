13eb06e56cf681ebf1fca2632ebf25573657ff77
akashkumar20 <akash@iofactory.in>
2020-01-26 02:29:56 +0530

    for git page

--- src/App/Screens/index.tsx
+++ src/App/Screens/index.tsx
@@ -1,20 +1,31 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+// import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Tabs from '../Components/Tabs';
+// import Tabs from '../Components/Tabs';
 import Home from './Home';
 import AboutMe from './AboutMe';
 // import ContactMe from './ContactMe';
 import MyPortfolio from './MyPortfolio';
 
 interface IContainerProps {}
-class Container extends React.Component<IContainerProps> {
+class Container extends React.Component<IContainerProps, any> {
     constructor(props:IContainerProps) {
         super(props);
-        this.state = {};
+        this.state = {
+            active: 0,
+        };
     }
 
-    render() {
+    onClick(i:number) {
+        this.setState((pre:any) => ({
+            ...pre,
+            active: i,
+        }));
+    }
+
+    renderTabs() {
+        // const { tabs, history } = this.props;
+        const { active } = this.state;
         const tabs = [
             {
                 tab: 'Home',
@@ -33,18 +44,53 @@ class Container extends React.Component<IContainerProps> {
                 route: 'aboutMe',
             },
         ];
+        return (
+            <div className="tab-main-box">
+                {
+                    tabs.map((tab:any, i:number) => (
+                        <div
+                            role="button"
+                            tabIndex={0}
+                            className={`tab ${active === i ? 'active' : ''}`}
+                            key={tab.tab}
+                            onClick={() => this.onClick(i)}
+                            onKeyPress={() => (console.log())}
+                        >
+                            <span>{tab.tab}</span>
+                        </div>
+                    ))
+                }
+            </div>
+        );
+    }
+
+    renderComponent() {
+        const { active } = this.state;
+        if (active === 1) {
+            return <MyPortfolio />;
+        }
+        if (active === 2) {
+            return <AboutMe />;
+        }
+        return <Home />;
+    }
+
+    render() {
         // const background = 'https://i.pinimg.com/originals/b1/55/ce/b155cec4a811dd6c8a840a89e0c03fb0.jpg';
         return (
             <div className="container">
-                <Tabs tabs={tabs} />
-                <BrowserRouter>
+                <div>
+                    {this.renderTabs()}
+                </div>
+                {/* <BrowserRouter>
                     <Switch>
                         <Route exact path="/AkashKumar" component={Home} />
-                        {/* <Route exact path="/contactMe" component={ContactMe} /> */}
+                        <Route exact path="/contactMe" component={ContactMe} />
                         <Route exact path="/AkashKumar/myPortfolio" component={MyPortfolio} />
                         <Route exact path="/AkashKumar/aboutMe" component={AboutMe} />
                     </Switch>
-                </BrowserRouter>
+                </BrowserRouter> */}
+                {this.renderComponent()}
             </div>
         );
     }
